706d24b9da48c0a3de4f63337a2ba593bcff3e0c
Bruno B. Zanella <bruno.zanella2015@outlook.com>
2020-06-13 22:46:19 -0300

    first-commit

---
+++ mobile/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>App route-64</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
